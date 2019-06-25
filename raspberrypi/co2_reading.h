bed32296e50cd2e485f928594f46346ef3e0eea6
fredoleary <fred@essentel.com>
2019-06-25 10:16:55 -0700

    Added random number generator

--- raspberrypi/co2_reading.h
+++ raspberrypi/co2_reading.h
@@ -1,7 +1,14 @@
+#include <random>
 #include "reading.h"
 
 class CO2Reading : public Reading{
+	
+	private:
+		std::mt19937 generator;
+		std::uniform_int_distribution<int> distribution;
+
 	public:
+		CO2Reading();
 		~CO2Reading();
 		std::string getName() const;
 		double getValue();
