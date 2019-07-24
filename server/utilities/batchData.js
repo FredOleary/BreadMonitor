a1e796bc39cae26b9792bb2af8352e544182db72
fredoleary <fred@essentel.com>
2019-07-24 11:44:51 -0700

    Added fermentation detection and sms message

--- server/utilities/batchData.js
+++ server/utilities/batchData.js
@@ -1,8 +1,12 @@
+const db = require('../models/index');
+const Reading = db.sequelize.models.Reading;
+var smsMessage = require( '../utilities/twilio')
+
 var BatchDictionary = {};
 var batchData ={};
 
 batchData.addBatch = ( batch) =>{
-    let batchInfo = {name:batch.name, alertTimes: [batch.createdAt]};
+    let batchInfo = {batch:batch, alertTimes: [batch.createdAt], alertMsgSent:false};
     BatchDictionary[batch.id] = batchInfo;
 };
 
@@ -13,9 +17,58 @@ batchData.update = (batchId) => {
         let diffTime = (nowDateTime.getTime() - batchInfo.alertTimes[batchInfo.alertTimes.length-1])/1000;
         if( diffTime > 60){     // Every minute
             batchInfo.alertTimes.push(nowDateTime );
-            console.log( "----- Minute tick....");
-        }
+            console.log( "----- Minute tick - checking for fermentation complete");
+            if( !batchInfo.alertMsgSent ){
+                Reading.findAll({
+                    raw: true,
+                    where:{BatchId:batchInfo.batch.id}
+                }).then(function(readings){
+                    batchData.checkFermentation( batchInfo, readings);
+                }).catch( err =>{
+                    console.log(err);
+                })
+            }
+         }
 
     }
+}
+// Empirically, fermentation is complete when a series of delta readings of greater than 
+// 1000 are detected. (This may result from CO2 bubbles bursting)
+// Note that readings from the CO2 sensor are in ppm/100. E.g. a reading from the sensor
+// of 61 corresponds to a C02 ppm value of 610
+const deltaReadingThreshold = 1000;
+const noOfDeltas = 2;
+batchData.checkFermentation =(batchInfo, readings) => {
+
+    deltaReadings = readings.map( (current, index, array) =>{
+        if( index > 0 ){
+            return Math.abs(array[index].co2 - array[index-1].co2);
+        }else{
+            return 0;
+        }
+    });
+    let idx = 0;
+    let count = 0;
+    while( idx < deltaReadings.length){
+        if( deltaReadings[idx] > deltaReadingThreshold){
+            count++;
+            if( count >= noOfDeltas){
+                batchInfo.alertMsgSent = true;
+                // Send a message
+                let fermentationStartDate = new Date( readings[0].createdAt);
+                let fermentationEndDate = new Date( readings[idx].createdAt);
+
+                let fermentationTimeInMinutes = (fermentationEndDate.getTime()- fermentationStartDate.getTime())/(60*1000);
+                let message = "Batch:" + batchInfo.batch.name + ". Fermentation complete at " + fermentationEndDate.toLocaleString() + 
+                 " ("  + Math.round(fermentationTimeInMinutes) + "mins)";
+                console.log( message);
+                smsMessage.sendMessage( message );
+                break;
+            }
+        }
+        idx++;
+    }
+    
+
 }
 module.exports = batchData;
